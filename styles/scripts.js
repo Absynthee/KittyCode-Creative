be81a2335bd4976c6a50ed64c7ce180a29284137
Austin Spillman <151957257+Absynthee@users.noreply.github.com>
2024-07-22 23:20:47 +0100

    commit

--- styles/scripts.js
+++ styles/scripts.js
@@ -312,7 +312,7 @@ function fade($ele) {
 }
 fade($(".quoteLoop > .quote").first());
 
-// form
+// contact form
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('Script is running');
@@ -409,6 +409,219 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// logo questionnaire form
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('Script is running');
+
+  const form = document.getElementById("logo");
+  const result = document.getElementById("result");
+
+  form.addEventListener("submit", function (event) {
+    console.log("Form submit event triggered");
+    event.preventDefault(); // Prevent form submission for testing
+
+    // Validation logic
+    let isValid = true;
+
+    // Name validation
+    const name = document.getElementById("name").value;
+    if (!name) {
+      document.getElementById("name-invalid").style.display = "block";
+      isValid = false;
+      console.log("Name is invalid");
+    } else {
+      document.getElementById("name-invalid").style.display = "none";
+    }
+
+    const businessname = document.getElementById("business-name").value;
+    if (!businessname) {
+      document.getElementById("business-name-invalid").style.display = "block";
+      isValid = false;
+      console.log("Business name is invalid");
+    } else {
+      document.getElementById("business-name-invalid").style.display = "none";
+    }
+
+
+    // Email validation
+    const email = document.getElementById("email").value;
+    const confirmemail = document.getElementById("confirm-email").value;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!email || !emailPattern.test(email)) {
+      document.getElementById("email-invalid").style.display = "block";
+      isValid = false;
+      console.log("Email is invalid");
+    } else {
+      document.getElementById("email-invalid").style.display = "none";
+    }
+
+    if (email.value !== confirmemail.value) {
+      document.getElementById("confirm-email-invalid").style.display = "block";
+      isValid = false;
+      console.log("Email confirmation is invalid");
+    } else {
+      document.getElementById("confirm-email-invalid").style.display = "none";
+    }    
+
+
+    // Message validation
+    const businessmessage1 = document.getElementById("what-does-your-business-do").value;
+    const businessmessage2 = document.getElementById("what-does-your-logo-achieve").value;
+    const businessmessage3 = document.getElementById("potential-customers").value;
+    const businessmessage4 = document.getElementById("differ-from-competitors").value;
+    const businessmessage5 = document.getElementById("main-competitors").value;
+    const logomessage1 = document.getElementById("logo-idea").value;
+    const logomessage2 = document.getElementById("logo-colours").value;
+    const logomessage3 = document.getElementById("logo-tagline").value;
+    const logomessage4 = document.getElementById("logo-emotions").value;
+    const logomessage5 = document.getElementById("logo-type").value;
+    const logomessage6 = document.getElementById("logo-appeal").value;
+    const logomessage7 = document.getElementById("logo-budget").value;
+
+    if (!businessmessage1) {
+      document.getElementById("business-1-invalid").style.display = "block";
+      isValid = false;
+      console.log("Business Message 1 is invalid");
+    } else {
+      document.getElementById("business-1-invalid").style.display = "none";
+    }
+
+    if (!businessmessage2) {
+      document.getElementById("business-2-invalid").style.display = "block";
+      isValid = false;
+      console.log("Business Message 2 is invalid");
+    } else {
+      document.getElementById("business-2-invalid").style.display = "none";
+    }
+
+    if (!businessmessage3) {
+      document.getElementById("business-3-invalid").style.display = "block";
+      isValid = false;
+      console.log("Business Message 3 is invalid");
+    } else {
+      document.getElementById("business-3-invalid").style.display = "none";
+    }
+
+    if (!businessmessage4) {
+      document.getElementById("business-4-invalid").style.display = "block";
+      isValid = false;
+      console.log("Business Message 4 is invalid");
+    } else {
+      document.getElementById("business-4-invalid").style.display = "none";
+    }
+
+    if (!businessmessage5) {
+      document.getElementById("business-5-invalid").style.display = "block";
+      isValid = false;
+      console.log("Business Message 5 is invalid");
+    } else {
+      document.getElementById("business-5-invalid").style.display = "none";
+    }
+
+    if (!logomessage1) {
+      document.getElementById("logo-1-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 1 is invalid");
+    } else {
+      document.getElementById("logo-1-invalid").style.display = "none";
+    }
+
+    if (!logomessage2) {
+      document.getElementById("logo-2-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 2 is invalid");
+    } else {
+      document.getElementById("logo-2-invalid").style.display = "none";
+    }
+
+    if (!logomessage3) {
+      document.getElementById("logo-3-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 3 is invalid");
+    } else {
+      document.getElementById("logo-3-invalid").style.display = "none";
+    }
+
+    if (!logomessage4) {
+      document.getElementById("logo-4-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 4 is invalid");
+    } else {
+      document.getElementById("logo-4-invalid").style.display = "none";
+    }
+
+    if (!logomessage5) {
+      document.getElementById("logo-5-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 5 is invalid");
+    } else {
+      document.getElementById("logo-5-invalid").style.display = "none";
+    }
+
+    if (!logomessage6) {
+      document.getElementById("logo-6-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 6 is invalid");
+    } else {
+      document.getElementById("logo-6-invalid").style.display = "none";
+    }
+
+    if (!logomessage7) {
+      document.getElementById("logo-7-invalid").style.display = "block";
+      isValid = false;
+      console.log("Logo Message 7 is invalid");
+    } else {
+      document.getElementById("logo-7-invalid").style.display = "none";
+    }
+
+
+
+
+    if (isValid) {
+      console.log("Form is valid");
+      // Proceed with form submission
+      const formData = new FormData(form);
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+      result.innerHTML = "Please wait...";
+
+      fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      })
+        .then(async (response) => {
+          let json = await response.json();
+          if (response.status == 200) {
+            result.innerHTML =
+              "Thanks, your message has been submitted! We'll get back to you as soon as possible.";
+          } else {
+            console.log(response);
+            result.innerHTML = json.message;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+          form.reset();
+          setTimeout(() => {
+            result.style.display = "none";
+          }, 10000);
+        });
+    } else {
+      console.log("Form is invalid. Make sure to fill out all fields.");
+    }
+  });
+});
+
 
 // button enabler
 
