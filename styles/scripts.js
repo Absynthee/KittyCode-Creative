2215489241befb4b0c1bdd977d8de5953a08c2af
Austin Spillman <151957257+Absynthee@users.noreply.github.com>
2024-09-03 20:30:53 +0100

    Added Dark Mode

--- styles/scripts.js
+++ styles/scripts.js
@@ -210,72 +210,6 @@ function translatePaymentTypeOneOff() {
   });
 }
 
-// services card hover
-// uses too many resources
-
-// const $primary = document.querySelector(".primary");
-
-// /**
-//  * return a value that has been rounded to a set precision
-//  * @param {Number} value the value to round
-//  * @param {Number} precision the precision (decimal places), default: 3
-//  * @returns {Number}
-//  */
-// const round = (value, precision = 3) => parseFloat(value.toFixed(precision));
-
-// /**
-//  * return a value that has been limited between min & max
-//  * @param {Number} value the value to clamp
-//  * @param {Number} min minimum value to allow, default: 0
-//  * @param {Number} max maximum value to allow, default: 100
-//  * @returns {Number}
-//  */
-// const clamp = (value, min = 0, max = 100) => {
-//   return Math.min(Math.max(value, min), max);
-// };
-
-// /**
-//  * return a value that has been re-mapped according to the from/to
-//  * - for example, adjust(10, 0, 100, 100, 0) = 90
-//  * @param {Number} value the value to re-map (or adjust)
-//  * @param {Number} fromMin min value to re-map from
-//  * @param {Number} fromMax max value to re-map from
-//  * @param {Number} toMin min value to re-map to
-//  * @param {Number} toMax max value to re-map to
-//  * @returns {Number}
-//  */
-// const adjust = (value, fromMin, fromMax, toMin, toMax) => {
-//   return round(
-//     toMin + ((toMax - toMin) * (value - fromMin)) / (fromMax - fromMin)
-//   );
-// };
-
-// const cardUpdate = (e) => {
-//   // normalise touch/mouse
-//   var pos = [e.clientX, e.clientY];
-//   e.preventDefault();
-//   if (e.type === "touchmove") {
-//     pos = [e.touches[0].clientX, e.touches[0].clientY];
-//   }
-//   var dimensions = $primary.getBoundingClientRect();
-//   var l = pos[0] - dimensions.left;
-//   var t = pos[1] - dimensions.top;
-//   var h = dimensions.height;
-//   var w = dimensions.width;
-//   var px = clamp(Math.abs((100 / w) * l), 0, 100);
-//   var py = clamp(Math.abs((100 / h) * t), 0, 100);
-
-//   $primary.setAttribute(
-//     "style",
-//     `
-//       --pointer-x: ${px}%;
-//       --pointer-y: ${py}%;
-//     `
-//   );
-// };
-
-// $primary.addEventListener("mousemove", cardUpdate);
-// $primary.addEventListener("touchmove", cardUpdate);
 
 // FAQ
 
@@ -539,7 +473,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const logomessage2 = document.getElementById("logo-colours").value;
     const logomessage3 = document.getElementById("logo-tagline").value;
     const logomessage4 = document.getElementById("logo-emotions").value;
-    const logomessage5 = document.getElementById("logo-type").value;
+    // const logomessage5 = document.getElementById("logo-type").value;
     const logomessage6 = document.getElementById("logo-appeal").value;
     const logomessage7 = document.getElementById("logo-budget").value;
 
@@ -615,13 +549,13 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("logo-4-invalid").style.display = "none";
     }
 
-    if (!logomessage5) {
-      document.getElementById("logo-5-invalid").style.display = "block";
-      isValid = false;
-      console.log("Logo Message 5 is invalid");
-    } else {
-      document.getElementById("logo-5-invalid").style.display = "none";
-    }
+    // if (!logomessage5) {
+    //   document.getElementById("logo-5-invalid").style.display = "block";
+    //   isValid = false;
+    //   console.log("Logo Message 5 is invalid");
+    // } else {
+    //   document.getElementById("logo-5-invalid").style.display = "none";
+    // }
 
     if (!logomessage6) {
       document.getElementById("logo-6-invalid").style.display = "block";
@@ -982,4 +916,27 @@ window.addEventListener("orientationchange", () => {
   if (isMobileDevice()) {
     removeTiltFromDivs();
   }
+});
+
+
+// Toggle theme between light & dark
+const themeToggle = document.getElementById("theme-toggle");
+const themeIcon = document.getElementById("theme-icon");
+const htmlElement = document.documentElement;
+
+const preferredTheme = localStorage.getItem("theme") || "light";
+htmlElement.classList.add(
+  preferredTheme === "dark" ? "dark-theme" : "light-theme"
+);
+
+themeToggle.addEventListener("click", () => {
+  console.log("Theme toggled");
+  htmlElement.classList.toggle("light-theme");
+  htmlElement.classList.toggle("dark-theme");
+
+  const newTheme = htmlElement.classList.contains("dark-theme")
+    ? "dark"
+    : "light";
+  localStorage.setItem("theme", newTheme);
+
 });
