c653ad874dce6cfd57d0daa6a02ed3013abb26e9
Austin <151957257+Absynthee@users.noreply.github.com>
2026-07-21 00:36:37 +0100

    delete unused components

--- src/components/ui/tooltip/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Tooltip } from "./Tooltip.astro";
-export { default as TooltipTrigger } from "./TooltipTrigger.astro";
-export { default as TooltipContent } from "./TooltipContent.astro";
